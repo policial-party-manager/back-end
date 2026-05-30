659918f837b72dada0a74454fff80e9642138c1d
drayee <1473443474@qq.com>
2026-05-30 16:14:22 +0800

    build:创建项目

---
+++ src/main/java/sicau/policialPartyManager/PolicialPartyManagerApplication.java
@@ -0,0 +1,13 @@
+package sicau.policialPartyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicialPartyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicialPartyManagerApplication.class, args);
+    }
+
+}
